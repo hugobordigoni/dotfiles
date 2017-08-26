0c3b09870d9aa29c281c19bc437bba1981f73368
Hugo Bordigoni <hugo.bordigoni@skillcorner.com>
2017-08-26 11:48:54 +0200

    add CUDA to path + better settings with GNOME

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,9 +4,11 @@ if [ "$(uname)" == "Darwin" ]; then
 	brew install tmux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	sudo apt-get install libevent-dev ncurses-dev
-	if [[ `tmux -V` == *master ]] then
+	if [[ `tmux -V` == *master ]]; then
 		git clone https://github.com/tmux/tmux $HOME/.dotfiles/tmux/src/
 		cd $HOME/.dotfiles/tmux/src
+		# Install version 2.4 using git tag. (For tmuxinator compatibility)
+		git checkout tags/2.4 -b 2.4
 		sh autogen.sh
 		./configure && make
 		cp tmux /bin
